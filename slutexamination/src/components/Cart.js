07e275e10753e2e7b9fdf4df50e7ddeb459431bd
Julia <julia.lissel@sunne.se>
2022-03-18 15:49:11 +0100

    added

--- slutexamination/src/components/Cart.js
+++ slutexamination/src/components/Cart.js
@@ -24,18 +24,33 @@ function Cart(){
     //     return totalSum += price
     //     },0)
    
-        const totalPrice = choies.reduce((totalSum, product) => {
+        let totalPrice = choies.reduce((totalSum, product) => {
             let price = product.item.price;
             return (totalSum += price * (product?.quantity || 1));
           }, 0);
-        const kampanj = choies.find((item,index) => {
-            console.log(item.item.id)
-            if(item.item.id === 1 && item.item.id === 7){
-                console.log('WINNWE');
-            } 
+        // const kampanj = choies.find((item,index) => {
+        //     console.log(item.item.id)
+        //     let newArr = [];
+        //     newArr.push(item.item.id)
+        //     // console.log(newArr)
+        //     if(item.item.id === 1 && item.item.id === 7){
+        //         console.log('WINNWE');
+        //     } 
+        // })
+        var newArray = choies.map(function(singleElement){
+            return singleElement.item.id;
         })
         
-        console.log(choies);
+        
+        //Här blir det även sant även om flera utöver 1 och 7
+        const res = newArray.includes(1) && newArray.includes(7) ;
+        console.log(res)
+        if(res){
+            totalPrice = 40
+        }
+
+        console.log(res);   
+        // console.log(choies);
     async function sendData(){
         dispatch(checkFetching(true))
         const respons = await fetch('http://localhost:5001/api/beans/', {
